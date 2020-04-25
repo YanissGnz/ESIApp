e5170046874a0347528c283254379b7e117d58f3
YanissGnz <guendouziyaniss@gmail.com>
2020-04-25 15:40:45 +0100

    last version

--- app/src/main/java/com/example/esiapp/adapters/PostDetail.java
+++ app/src/main/java/com/example/esiapp/adapters/PostDetail.java
@@ -3,15 +3,25 @@ package com.example.esiapp.adapters;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.esiapp.R;
 
 public class PostDetail extends AppCompatActivity {
+
+    ImageView postPicture;
+    EditText commentText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_detail);
 
+        postPicture = findViewById(R.id.post_detail_picture);
+        postPicture.setMaxHeight(500);
+
+        commentText = findViewById(R.id.post_detail_comment_text);
+        commentText.setMaxHeight(300);
     }
 }
